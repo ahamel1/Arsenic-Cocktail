9d94910509b5c414146b5ba5aec6d589c038d6af
lebowvsky <lebowvsky@gmail.com>
2020-04-15 09:52:18 +0200

    add dependencies needed to the package.json and import them to the App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
