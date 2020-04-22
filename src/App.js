ce19d401b377603b59425771cb0a51a690f8f26b
erwanitwish <quillec.erwan@gmail.com>
2020-04-22 10:23:18 +0200

    navBar component creation with semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import AlgoRandom from './components/AlgoRandom';
+import NavBar from './components/NavBar';
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
 
 function App() {
     return (
       <div className="App">
-          <AlgoRandom />
+        <div class="navbar-position">
+          <NavBar />
+        </div>
+        <AlgoRandom />
       </div>
     );
-  
+
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Icon, Menu } from 'semantic-ui-react'
+
+class NavBar extends React.Component {
+  state = { activeItem: '' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+    const menuStyle = {
+      boxShadow : "none",
+      border : 'none'
+    }
+
+    return (
+      <Menu borderless icon style= {menuStyle}>
+        <Menu.Item
+          name='return'
+          active={activeItem === 'return'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='arrow left' />
+        </Menu.Item>
+
+        <Menu.Menu position= "right">
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='home' />
+        </Menu.Item>
+
+        <Menu.Item
+          name='info'
+          active={activeItem === 'info'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='info' />
+        </Menu.Item>
+
+        <Menu.Item
+          name='at'
+          active={activeItem === 'at'}
+          onClick={this.handleItemClick}
+        >
+          <Icon name='at' />
+        </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
+
+export default NavBar;
