0a55746cc32f9a255a55f01dd7cf3e8fc2317d4d
ahamel1 <ahamel.wcs44@gmail.com>
2020-04-22 10:24:29 +0200

    delete unused files, create home page with router and components

---
+++ src/components/HomePart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import TitleCpnt from './TitleCpnt'
+
+function HomePart ({ title, buttonTexte, linkTo, imageHomeSrc, imageHomeAlt }) {
+  return (
+    <div>
+      <img src={imageHomeSrc} alt={imageHomeAlt} />
+      <TitleCpnt title={title} />
+      <Link to={linkTo} ><button>{buttonTexte}</button></Link>
+    </div>
+  )
+}
+
+export default HomePart;
