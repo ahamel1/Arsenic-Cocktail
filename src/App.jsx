4b93365d5dadd347692efc932b7105de51ff663b
erwanitwish <quillec.erwan@gmail.com>
2020-04-24 15:12:15 +0200

    double home import deleted in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+    return (
+      <div className='App'>
+        <Router>
+          <div className='navbar-position'>
+            <NavBar />
+          </div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomePart from './HomePart';
+
+function Home () {
+  return (
+    <div className='Home'>
+      <HomePart title='ALCOOL' buttonTexte="J'SUIS CHAUD" linkTo='/alcool' imageHomeSrc='' imageHomeAlt='shaker' />
+      <HomePart title='FOLIE' buttonTexte="J'SUIS CHAUD" linkTo='/folie' imageHomeSrc='' imageHomeAlt='piment' />
+      <HomePart title='SOFT' buttonTexte="J'SUIS CHAUD" linkTo='/soft' imageHomeSrc='' imageHomeAlt='banane' />
+    </div>
+  );
+  
+}
+
+export default Home;
