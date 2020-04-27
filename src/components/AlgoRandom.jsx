c797e12f12bcb5a00bfec1a51df5c03c5a95f232
erwanitwish <quillec.erwan@gmail.com>
2020-04-27 11:54:54 +0200

    home style & algo style done

--- src/components/AlgoRandom.jsx
+++ src/components/AlgoRandom.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ListIngredients from './ListIngredients';
 import axios from 'axios';
-import Button from './Button';
+import ButtonCpnt from './ButtonCpnt';
+import TitleCpnt from './TitleCpnt';
 
 class AlgoRandom extends React.Component {
   constructor (props) {
@@ -40,10 +41,10 @@ class AlgoRandom extends React.Component {
 
   render () {
     return (
-      <div className='AlgoRandom container'>
+      <div className='algoRandom container'>
         <div className='part button-part'>
-          <h1>La folie</h1>
-          <Button onClick={this.getList}>Générer</Button>
+          <TitleCpnt title="LA FOLIE" />
+          <ButtonCpnt className='button-cpnt' onClick={this.getList}>Générer</ButtonCpnt>
         </div>
         <div className='part'>
           <ListIngredients list={this.state.list} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import HomePart from './HomePart';
+import piment from './piment.png';
+import shaker from './shaker.png';
+import banane from './banane.png';
 
 function Home () {
   return (
     <div className='Home container'>
-      <HomePart className='alcool' title='ALCOOL' buttonTexte="J'SUIS CHAUD" linkTo='/alcool' imageHomeSrc='' imageHomeAlt='shaker' />
-      <HomePart className='la-folie' title='FOLIE' buttonTexte="J'SUIS CHAUD" linkTo='/folie' imageHomeSrc='' imageHomeAlt='piment' />
-      <HomePart className='sans-alcool' title='SOFT' buttonTexte="J'SUIS CHAUD" linkTo='/soft' imageHomeSrc='' imageHomeAlt='banane' />
+      <HomePart className='alcool homepart-position' title='ALCOOL' buttonTexte="J'SUIS CHAUD" linkTo='/alcool' imageHomeSrc={shaker} imageHomeAlt='shaker' />
+      <HomePart className='la-folie homepart-position' title='FOLIE' buttonTexte="J'SUIS CHAUD" linkTo='/folie' imageHomeSrc={piment} imageHomeAlt='piment' />
+      <HomePart className='sans-alcool homepart-position' title='SANS ALCOOL' buttonTexte="J'SUIS CHAUD" linkTo='/soft' imageHomeSrc={banane} imageHomeAlt='banane' />
     </div>
   );
 }
