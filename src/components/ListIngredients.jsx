873e9255437153244cc18520615b84493245afc7
erwanitwish <quillec.erwan@gmail.com>
2020-04-28 11:08:27 +0200

    animation list random

--- src/components/ListIngredients.jsx
+++ src/components/ListIngredients.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTrail, animated } from 'react-spring'
+
+const config = { mass: 5, tension: 2000, friction: 200 }
+
 
 function ListIngredients ({ list }) {
-  return (
-    <ul>
-      {list.map((item, index) => {
-        return <li key={index}> {item}</li>;
-      })}
-    </ul>
-  );
+  const [toggle, set] = useState(true)
+    const trail = useTrail(list.length, {
+      config,
+      opacity: toggle ? 1 : 0,
+      x: toggle ? 0 : 20,
+      height: toggle ? 80 : 0,
+      from: { opacity: 0, x: 20, height: 0 },
+    })
+
+    return (
+      <div className="trails-main">
+        <div>
+          {trail.map(({ x, height, ...rest }, index) => (
+            <animated.ul
+              key={list[index]}
+              className="trails-text"
+              style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+              <animated.li style={{ height }}>{list[index]}</animated.li>
+            </animated.ul>
+          ))}
+        </div>
+      </div>
+    )
 }
 
 export default ListIngredients;
