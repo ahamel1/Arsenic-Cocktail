b99cb398699dac8416a03aedf18259e62b745724
lebowvsky <lebowvsky@gmail.com>
2020-05-05 17:22:11 +0200

    animations done

--- src/components/ListIngredients.jsx
+++ src/components/ListIngredients.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { useTrail, animated, config } from 'react-spring';
+import React, { useState } from 'react';
+import { useTrail, animated } from 'react-spring'
+
+const config = { mass: 5, tension: 2000, friction: 200 }
+
 
 function ListIngredients ({ list }) {
+  const [toggle, set] = useState(true)
   const trail = useTrail(list.length, {
-    config: config.wobbly,
-    transform: 'translateY(0vh)',
-    from: { transform: 'translateY(-70vh)' }
-  });
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 20,
+    height: toggle ? 80 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+
   return (
-    <div>
-      {trail.map(({...rest }, index) => (
-        <ul key={list[index]}>
-          <animated.li style={{ ...rest }}>{list[index]}</animated.li>
-        </ul>
-      ))}
-    </div>
-  );
+      <div className="trails-main">
+        <div>
+          {trail.map(({ x, height, ...rest }, index) => (
+              <animated.ul
+                  key={list[index]}
+                  className="trails-text"
+                  style={{ ...rest, transform: x.interpolate(x => `translate3d(0,${x}px,0)`) }}>
+                <animated.li style={{ height }}>{list[index]}</animated.li>
+              </animated.ul>
+          ))}
+        </div>
+      </div>
+  )
 }
+
 export default ListIngredients;
