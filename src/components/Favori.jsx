2374434de31576119120a1dadcc8db752ee04e40
lebowvsky <lebowvsky@gmail.com>
2020-05-14 00:12:13 +0200

    little (Yes! I said a little...) animation on Favori...

--- src/components/Favori.jsx
+++ src/components/Favori.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useTrail, animated, config } from 'react-spring';
 
 const Favori = () => {
   const [recipe, setRecipe] = useState(['Votre liste est vide...']);
@@ -8,6 +9,26 @@ const Favori = () => {
     }
   }, []);
 
+  const recipeWithId = recipe.map((item, i) => {
+    return { id: i, name: item };
+  });
+  const [toggle] = useState(true);
+  const trail = useTrail(recipeWithId.length, {
+    config: config.slow,
+    opacity: toggle ? 1 : 0,
+    left: '0px',
+    from: { opacity: 0, left: '-150px' },
+    delay: 300,
+  });
+
+  const trail2 = useTrail(recipeWithId.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    left: '0px',
+    from: { opacity: 0, left: '150px' },
+    delay: 300,
+  });
+
   return (
     <div className="container container-mobile">
       <div className="alcool alcool-other-pages" />
@@ -16,9 +37,20 @@ const Favori = () => {
       <div id="text-over">
         <h2>Favori</h2>
         <ul>
-          {recipe.map((elt) => (
+          {trail.map((animation, index) =>
+            index % 2 === 0 ? (
+              <animated.li style={animation} key={recipe[index]}>
+                {recipe[index]}
+              </animated.li>
+            ) : (
+              <animated.li style={trail2[index]} key={recipe[index]}>
+                {recipe[index]}
+              </animated.li>
+            )
+          )}
+          {/* {recipe.map((elt) => (
             <li>{elt}</li>
-          ))}
+          ))} */}
         </ul>
       </div>
     </div>
